da734ab87b9d3996486eb194fad18886b4231035
Shams Shimul <msr.shimul@gmail.com>
2021-11-30 12:50:08 +0600

    Login page refactored

--- pages/login.js
+++ pages/login.js
@@ -1,26 +1,31 @@
-import Link from "next/link";
-import { Button, Container, Grid } from "@mui/material";
-import Typography from "@mui/material/Typography";
+import {
+  Container,
+  Grid,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Typography,
+} from "@mui/material";
+import Link from "@mui/material/Link";
 import styles from "../styles/Login.module.css";
-import Image from "next/image";
-import { Box } from "@mui/system";
 import CheckIcon from "@mui/icons-material/Check";
 import CloseIcon from "@mui/icons-material/Close";
+import Image from "next/image";
+import LoginForm from "../components/LoginForm";
+
 import { useState } from "react";
 
 export default function Login() {
-  const [isChecked, setIsChecked] = useState(false);
   const [usrEmail, setUsrEmail] = useState("");
   const [usrPsw, setUsrPsw] = useState("");
+  const [isChecked, setIsChecked] = useState(false);
   const [forgotPsw, setForgotPsw] = useState(false);
 
-  const toggleCheck = () => {
-    setIsChecked(!isChecked);
-  };
-
-  const toggleForgotPsw = () => {
+  const toggleForgotPsw = (e) => {
+    e.preventDefault();
     setForgotPsw(!forgotPsw);
-    console.log(forgotPsw);
+    setUsrEmail("");
+    setUsrPsw("");
   };
 
   const handleForm = (e) => {
@@ -31,121 +36,166 @@ export default function Login() {
   };
 
   return (
-    <div className={styles.login_bg}>
+    <div className={styles.loginBg}>
       <Container fixed>
-        <Grid container className={styles.login_card} spacing={0}>
-          <Grid item md={6} className={styles.login_card_left}>
-            <div className={styles.logo}>
-              <Image
-                src="/Relisource_logo_white-01-1.png"
-                width={128}
-                height={49}
-                alt="Relisource Logo"
-              />
-              <sup className={styles.logo_subtitle}>ERP</sup>
-            </div>
-            <h1 className={styles.greetings}>Welcome</h1>
-          </Grid>
-          <Grid
-            item
-            md={6}
-            className={styles.login_card_right}
-            sx={{ background: "#fff" }}
-          >
-            <Typography
-              variant="h5"
-              color="#097ff5"
-              sx={{ fontFamily: "Rubik", fontWeight: "500", mb: "45px" }}
-            >
-              {forgotPsw ? `Enter Registered Email` : `Login Account`}
-            </Typography>
-            <form
-              onSubmit={handleForm}
-              className={styles.login_form}
-              style={{ width: "100%" }}
-            >
-              <input
-                type="email"
-                name="email"
-                id="email"
-                placeholder="Email ID"
-                className={styles.input_field}
-                value={usrEmail}
-                onChange={(e) => setUsrEmail(e.target.value)}
-              />
-              <input
-                type="password"
-                name="psw"
-                id="psw"
-                placeholder="Password"
-                className={styles.input_field}
-                style={forgotPsw ? { display: "none" } : { display: "initial" }}
-                value={usrPsw}
-                onChange={(e) => setUsrPsw(e.target.value)}
-              />
-              <Box
-                className={styles.login_helper}
-                sx={
-                  forgotPsw
-                    ? { justifyContent: "center" }
-                    : { justifyContent: "space-between" }
-                }
-              >
-                <Box
-                  sx={forgotPsw ? { display: "none" } : { display: "initial" }}
+        <div className={styles.loginCard}>
+          <Grid container spacing={0}>
+            <Grid item xs={12} md={6}>
+              <div className={styles.loginCardLeft}>
+                <div className={styles.logo}>
+                  <Image
+                    src="/Relisource_logo_white-01-1.png"
+                    width={128}
+                    height={49}
+                    alt="Relisource Logo"
+                  />
+                  <Typography component="sup" className={styles.logoSubtitle}>
+                    ERP
+                  </Typography>
+                </div>
+                <Typography
+                  variant="h4"
+                  component="h1"
+                  fontWeight="400"
+                  color="#f7f7f7"
+                >
+                  Hi, Welcome!
+                </Typography>
+              </div>
+            </Grid>
+            <Grid item xs={12} md={6} bgcolor="#fff">
+              <div className={styles.loginCardRight}>
+                <Typography
+                  variant="h5"
+                  color="#097ff5"
+                  fontFamily="Rubik"
+                  fontWeight="500"
+                  mb="45px"
+                >
+                  {forgotPsw ? `Enter Registered Email` : `Login Account`}
+                </Typography>
+
+                {/* Split the form part */}
+
+                {/* <LoginForm
+                  values={{
+                    usrEmail,
+                    setUsrEmail,
+                    usrPsw,
+                    setUsrPsw,
+                    isChecked,
+                    setIsChecked,
+                    forgotPsw,
+                    toggleForgotPsw,
+                    handleForm,
+                  }}
+                /> */}
+
+                <form
+                  autoComplete="off"
+                  className={styles.loginForm}
+                  style={{ width: "100%" }}
+                  noValidate
                 >
                   <input
-                    type="checkbox"
-                    name="stayLoggedIn"
-                    style={{ opacity: "0", marginRight: "10px" }}
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Email ID"
+                    className={styles.inputField}
+                    value={usrEmail}
+                    onChange={(e) => setUsrEmail(e.target.value)}
                   />
-                  <label
-                    htmlFor="stayLoggedIn"
-                    className={styles.stay_logged_in}
-                    onClick={toggleCheck}
-                    style={isChecked ? { color: "#097ff5" } : { color: "#ccc" }}
+                  <input
+                    type="password"
+                    name="psw"
+                    id="psw"
+                    placeholder="Password"
+                    className={styles.inputField}
+                    style={
+                      forgotPsw ? { display: "none" } : { display: "initial" }
+                    }
+                    value={usrPsw}
+                    onChange={(e) => setUsrPsw(e.target.value)}
+                  />
+                  <div className={styles.loginHelper}>
+                    <Grid
+                      container
+                      alignItems="center"
+                      sx={
+                        forgotPsw
+                          ? { justifyContent: "center" }
+                          : { justifyContent: "space-between" }
+                      }
+                    >
+                      <Grid
+                        item
+                        xs={12}
+                        md={6}
+                        textAlign="left"
+                        sx={
+                          forgotPsw
+                            ? { display: "none" }
+                            : { display: "initial" }
+                        }
+                        fontFamily="Rubik"
+                      >
+                        <FormControlLabel
+                          control={
+                            <Checkbox
+                              size="small"
+                              icon={<CloseIcon />}
+                              checkedIcon={<CheckIcon />}
+                              sx={{
+                                color: "#ccc",
+                              }}
+                            />
+                          }
+                          label={
+                            <Typography
+                              fontSize="0.9rem"
+                              fontFamily="Rubik"
+                              color={isChecked ? `blue` : `#ccc`}
+                              onClick={() => setIsChecked(!isChecked)}
+                            >
+                              Keep me signed in
+                            </Typography>
+                          }
+                        />
+                      </Grid>
+                      <Grid
+                        item
+                        xs={12}
+                        md={6}
+                        className={styles.forgotPsw}
+                        textAlign={forgotPsw ? `center` : `right`}
+                        onClick={toggleForgotPsw}
+                      >
+                        {forgotPsw ? (
+                          <Link href="/login" underline="none">
+                            Back to login
+                          </Link>
+                        ) : (
+                          <Link href="/login" underline="none">
+                            Forgot Password?
+                          </Link>
+                        )}
+                      </Grid>
+                    </Grid>
+                  </div>
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    className={styles.loginButton}
+                    onClick={handleForm}
                   >
-                    Keep me signed in
-                    <span
-                      className={styles.check_icon}
-                      style={
-                        isChecked
-                          ? { color: "#097ff5" }
-                          : { color: "transparent" }
-                      }
-                    >
-                      <CheckIcon />
-                    </span>
-                    <span
-                      className={styles.close_icon}
-                      style={
-                        isChecked ? { color: "transparent" } : { color: "#ccc" }
-                      }
-                    >
-                      <CloseIcon />
-                    </span>
-                  </label>
-                </Box>
-                <Box className={styles.forgot_psw} onClick={toggleForgotPsw}>
-                  {forgotPsw ? (
-                    <Link href="/login">
-                      <a>Back to login</a>
-                    </Link>
-                  ) : (
-                    <span>Fotgot Password?</span>
-                  )}
-                </Box>
-              </Box>
-              <Button
-                type="submit"
-                variant="contained"
-                className={styles.login_button}
-              >
-                {forgotPsw ? `Recover Password` : `Login`}
-              </Button>
-            </form>
+                    {forgotPsw ? `Recover Password` : `Login`}
+                  </Button>
+                </form>
+              </div>
+            </Grid>
           </Grid>
-        </Grid>
+        </div>
       </Container>
     </div>
   );
