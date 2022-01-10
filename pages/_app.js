c2a40e521cc7a767d4dd8b564526f3553e74bd75
Shams Shimul <msr.shimul@gmail.com>
2022-01-10 12:04:34 +0600

    forgot password separated

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
 import "../styles/globals.css";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <>
+      <Head>
+        <title>ReliSource ERP</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
