357a446594b7383722a86bf45548442235292c89
Shams Shimul <msr.shimul@gmail.com>
2022-01-19 01:56:29 +0600

    Layout and design changed
    Instead of custom designed HTML elements, MUI components have been used

--- pages/dashboard/add-new-account.js
+++ pages/dashboard/add-new-account.js
@@ -1,82 +1,153 @@
+import { useState } from "react";
 import Dashboard from "../../components/Layout/dashboard";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
 import FormControlLabel from "@mui/material/FormControlLabel";
-import FormHelperText from "@mui/material/FormHelperText";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import AppRegistrationIcon from "@mui/icons-material/AppRegistration";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import InputAdornment from "@mui/material/InputAdornment";
+import IconButton from "@mui/material/IconButton";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import Grid from "@mui/material/Grid";
 
 const AddNewAccount = () => {
+  const [showPassword, setShowPassword] = useState(false);
   return (
     <Dashboard>
-      <form noValidate autoComplete="off">
-        <input
-          type="text"
-          name="fullname"
-          id="fullname"
-          placeholder="Full Name"
-          className="inputField"
-        />
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Email"
-          className="inputField"
-        />
-        <input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Password"
-          className="inputField"
-        />
-        <input
-          type="tel"
-          name="phone"
-          id="phone"
-          placeholder="Mobile/Phone Number"
-          className="inputField"
-        />
-        <Box>
-          <FormControl>
-            <FormLabel>Assign Roles:</FormLabel>
-
-            <RadioGroup row aria-label="role" name="role" sx={{ ml: "10px" }}>
-              <FormControlLabel
-                value="role1"
-                control={<Radio />}
-                label="Role 1"
-                className="radio"
-              />
-              <FormControlLabel
-                value="role2"
-                control={<Radio />}
-                label="Role 2"
-                className="radio"
-              />
-              <FormControlLabel
-                value="role3"
-                control={<Radio />}
-                label="Role 3"
-                className="radio"
-              />
-            </RadioGroup>
-          </FormControl>
-        </Box>
-      </form>
-      <Button
-        type="submit"
-        variant="contained"
-        size="large"
-        sx={{ mt: 2 }}
-        startIcon={<AppRegistrationIcon />}
-      >
-        Add
-      </Button>
+      <Typography variant="h5" mb={3}>
+        Add New Account
+      </Typography>
+      <Box variant="form" noValidate autoComplete="off">
+        <Grid container columnSpacing={3}>
+          <Grid item xs={12} lg={6}>
+            <TextField
+              id="fullname"
+              name="fullname"
+              label="Full Name"
+              required
+              helperText=" "
+              fullWidth
+              sx={{ mb: 1 }}
+            />
+          </Grid>
+          <Grid item xs={12} lg={6}>
+            <TextField
+              inputProps={{ type: "email" }}
+              id="email"
+              name="email"
+              label="Email"
+              required
+              helperText=" "
+              fullWidth
+              sx={{ mb: 1 }}
+            />
+          </Grid>
+          <Grid item xs={12} lg={6}>
+            <TextField
+              inputProps={{ type: showPassword ? "text" : "password" }}
+              id="password"
+              name="password"
+              label="Password"
+              required
+              helperText=" "
+              fullWidth
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={() => setShowPassword(!showPassword)}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Grid>
+          <Grid item xs={12} lg={6}>
+            <TextField
+              inputProps={{ type: showPassword ? "text" : "password" }}
+              id="password"
+              name="password"
+              label="Retype Password"
+              required
+              helperText=" "
+              fullWidth
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={() => setShowPassword(!showPassword)}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Grid>
+          <Grid item xs={12} lg={6}>
+            <TextField
+              inputProps={{ type: "tel" }}
+              id="mobile"
+              name="mobile"
+              label="Mobile Number"
+              placeholder="01XXX-XXXXXX"
+              required
+              helperText=" "
+              fullWidth
+              sx={{ mb: 1 }}
+            />
+          </Grid>
+          <Grid item xs={12} lg={6}>
+            <FormControl fullWidth>
+              <FormLabel>Assign Roles:</FormLabel>
+              <RadioGroup row aria-label="role" name="role" sx={{ ml: "10px" }}>
+                <FormControlLabel
+                  value="user"
+                  control={<Radio />}
+                  label="User"
+                  className="radio"
+                />
+                <FormControlLabel
+                  value="admin"
+                  control={<Radio />}
+                  label="Admin"
+                  className="radio"
+                />
+                <FormControlLabel
+                  value="other"
+                  control={<Radio />}
+                  label="Other"
+                  className="radio"
+                />
+              </RadioGroup>
+            </FormControl>
+          </Grid>
+        </Grid>
+        <Button
+          type="submit"
+          variant="contained"
+          size="large"
+          sx={{
+            mt: 2,
+            borderRadius: 5,
+          }}
+          startIcon={<AppRegistrationIcon />}
+        >
+          Create Account
+        </Button>
+      </Box>
     </Dashboard>
   );
 };
